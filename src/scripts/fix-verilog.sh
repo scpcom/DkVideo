97adaa1da31ffd262375fefba7636ebd3d130e18
scpcom <scpcom@gmx.de>
2022-03-08 01:03:47 +0100

    fixing scl/sda is not needed

--- src/scripts/fix-verilog.sh
+++ src/scripts/fix-verilog.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # inout is unknown in chisel, one bit array also missing
-sed -i s/'output       SCL'/'inout        SCL'/g video_top.v
-sed -i s/'input        SDA'/'inout        SDA'/g video_top.v
 sed -i s/'input  .7.0. IO_hpram_dq'/'inout  [7:0] IO_hpram_dq'/g video_top.v
 #sed -i s/'input        IO_hpram_rwds'/'inout        IO_hpram_rwds'/g video_top.v
 sed -i s/'input        IO_hpram_rwds'/'inout  [0:0] IO_hpram_rwds'/g video_top.v
