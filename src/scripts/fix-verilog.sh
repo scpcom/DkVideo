3babc06e9b33b085bedcd45a5870574415cbc2d6
scpcom <scpcom@gmx.de>
2022-03-15 22:30:48 +0100

    scripts: fix-verilog: unwire not needed for  IO_hpram_dq

--- src/scripts/fix-verilog.sh
+++ src/scripts/fix-verilog.sh
@@ -6,10 +6,7 @@ sed -i s/'input  .7.0. IO_hpram_dq'/'inout  [7:0] IO_hpram_dq'/g video_top.v
 sed -i s/'input        IO_hpram_rwds'/'inout  [0:0] IO_hpram_rwds'/g video_top.v
 sed -i s/'output       O_hpram'/'output [0:0] O_hpram'/g video_top.v
 
-sed -i /'wire .7.0. '${hminame}'_IO_hpram_dq'/d video_top.v
-sed -i /'assign '${hminame}'_IO_hpram_dq = IO_hpram_dq;'/d video_top.v
-sed -i s/${hminame}'_IO_hpram_dq'/'IO_hpram_dq'/g video_top.v
-
+# unwire IO_hpram_rwds, connect directly
 sed -i /'wire  '${hminame}'_IO_hpram_rwds;'/d video_top.v
 sed -i /'wire .0.0. '${hminame}'_IO_hpram_rwds;'/d video_top.v
 sed -i /'assign '${hminame}'_IO_hpram_rwds = IO_hpram_rwds;'/d video_top.v
